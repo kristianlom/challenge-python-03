2363ffdb52d53d2fc0c9e08b5ce05150ac2da484
Kristian Lopez <kristian.lom@gmail.com>
2020-02-15 17:08:36 -0600

    Reto solucionado!

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 # Resolve the problem!!
+import io
+import re
 
 
 def run():
-    # Start coding here
+    with io.open("encoded.txt", "r", encoding='utf-8') as f:
+        y = re.compile('[a-z]').findall(f.readline())
+        print(''.join(y))
 
 
 if __name__ == '__main__':
